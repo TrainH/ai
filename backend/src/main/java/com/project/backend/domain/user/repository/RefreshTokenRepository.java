31917e8bb23b936882895a21d5e65cd784d2ecbb
TrainH <130670891+TrainH@users.noreply.github.com>
2026-05-15 14:59:35 +0900

    로그인 최적화

--- backend/src/main/java/com/project/backend/domain/user/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/project/backend/domain/user/repository/RefreshTokenRepository.java
@@ -2,10 +2,16 @@ package com.project.backend.domain.user.repository;
 
 import com.project.backend.domain.user.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByTokenHash(String tokenHash);
-    void deleteByUserId(Long userId);
+
+    @Modifying
+    @Query("delete from RefreshToken r where r.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 }
